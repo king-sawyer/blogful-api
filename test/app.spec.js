bb6f53f9a9d9a1c62222a3436a8592c6b8fd09bd
Sawyer King <kingsawyer555@gmail.com>
2020-11-22 20:37:06 -0800

    initial commit

--- test/app.spec.js
+++ test/app.spec.js
@@ -2,9 +2,9 @@
 const app = require('../src/app')
 
 describe('App', ()=> {
-    it('GET / reponds with 200 containing "Hello, world!"', () => {
+    it('GET / reponds with 200 and all the articles', () => {
         return supertest(app)
-        .get("/")
-        .expect(200, "Hello, world!")
+         .get("/")
+        .expect(200)
     });
 });
